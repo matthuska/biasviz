0c06dba8f7be8821cee47c2c7f0c6bd8bc6a4f2b
matthuska <matthuska@2b43fe30-8534-0410-aa73-fcb64b800238>
2007-11-09 17:51:57 +0000

    Can manipulate graphical view area using mouse: - Mouse wheel zooms in and out - Left click and drag to pan

--- trunk/src/java/GraphicView.java
+++ trunk/src/java/GraphicView.java
@@ -27,6 +27,7 @@
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class GraphicView extends JPanel implements IView {
 
@@ -38,11 +39,16 @@ public class GraphicView extends JPanel implements IView {
     SequenceLabels names;
     ZoomControlView zoomControls;
 
+    // Used for mouse panning of the view
+    int mouseLastX;
+
     public GraphicView(CompositionModel model) {
         assert model != null;
         this.model = model;
 
         this.layoutView();
+
+        this.registerControllers();
 
         this.model.addView(this);
 
@@ -57,6 +63,8 @@ public class GraphicView extends JPanel implements IView {
         this.plot.setBackground(Color.WHITE);
 
         this.scroll = new JScrollPane(plot);
+        // Use wheel for zoom instead of default action.
+        this.scroll.setWheelScrollingEnabled(false);
 
         this.aarule = new Rule(model, Rule.HORIZONTAL);
         this.names = new SequenceLabels(model);
@@ -73,5 +81,62 @@ public class GraphicView extends JPanel implements IView {
     public void updateView() {
     }
 
+    public void registerControllers() {
+
+        // Zoom using mouse wheel
+        // FIXME: Have zoom center on area where mouse pointer is.
+        this.plot.addMouseWheelListener(new MouseWheelListener()
+        {
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.getWheelRotation() < 0) {
+                    model.incZoomWidth();
+                } else {
+                    model.decZoomWidth();
+                }
+            }
+
+        });
+
+        // Click and drag to pan image
+        this.plot.addMouseMotionListener(new MouseMotionAdapter()
+        {
+            public void mouseDragged(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    int diff = e.getX() - mouseLastX;
+
+                    JViewport vp = scroll.getViewport();
+                    int newX = (int)vp.getViewPosition().getX() - diff;
+
+                    // Prevent from scrolling beyond ends of the plot/image
+                    if (newX < 0) {
+                        newX = 0;
+                    } else {
+                        int rightX = (int)plot.getPreferredSize().getWidth() - 
+                                scroll.getHorizontalScrollBar().getVisibleAmount();
+
+                        if (newX > rightX) {
+                            newX = rightX;
+                        }
+                    }
+
+                    scroll.getViewport().setViewPosition(new Point(newX, 0));
+
+                    // Need to subtract "diff" here because viewport has moved
+                    // and changed the location of the last mouse location
+                    // relative to the graphical view
+                    mouseLastX = e.getX() - diff;
+                }
+            }
+        });
+
+        this.plot.addMouseListener(new MouseAdapter()
+        {
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    mouseLastX = e.getX();
+                }
+            }
+        });
+    }
 }
 
